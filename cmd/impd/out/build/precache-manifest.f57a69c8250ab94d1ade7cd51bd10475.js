0477f1f88f138e281dbd9abc7a5e5a7051bfc332
markcryptohash <mark@impervious.ai>
2022-09-26 09:08:28 -0700

    Added client_addr to config.yml for port 8080

---
+++ cmd/impd/out/build/precache-manifest.f57a69c8250ab94d1ade7cd51bd10475.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feef3e8f8862f0ac7d48862a8d2b86be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5866156beb61326fdc2c",
+    "url": "/static/css/2.11a3a505.chunk.css"
+  },
+  {
+    "revision": "e41d63ed43820adf1ea2",
+    "url": "/static/css/main.fdf4a3e6.chunk.css"
+  },
+  {
+    "revision": "5866156beb61326fdc2c",
+    "url": "/static/js/2.341efe2a.chunk.js"
+  },
+  {
+    "revision": "dfb22c9f15b622c076b748a8239387dd",
+    "url": "/static/js/2.341efe2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41d63ed43820adf1ea2",
+    "url": "/static/js/main.5f5433a0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5f5433a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "610a19854b397da449700f35924e4015",
+    "url": "/static/media/1-panickypanda.610a1985.svg"
+  },
+  {
+    "revision": "791984d97f1aa7da9e88c9234048b5ca",
+    "url": "/static/media/10-lamellama.791984d9.svg"
+  },
+  {
+    "revision": "0f872eb53aa03e0b37c6ab8f6123a504",
+    "url": "/static/media/11-ableanteater.0f872eb5.svg"
+  },
+  {
+    "revision": "1db0df8288ffbf5e7bddbb6fe88aac27",
+    "url": "/static/media/12-edgyeagle.1db0df82.svg"
+  },
+  {
+    "revision": "142d5ec4a6ff733a806386f3538e68e7",
+    "url": "/static/media/13-crispycrocodile.142d5ec4.svg"
+  },
+  {
+    "revision": "479ba19acb592fccf28388741909b557",
+    "url": "/static/media/14-ballisticbeaver.479ba19a.svg"
+  },
+  {
+    "revision": "fa42a24b1ba7a16351ea13cae527721e",
+    "url": "/static/media/15-hastyhamster.fa42a24b.svg"
+  },
+  {
+    "revision": "7079ad99d24bb86331a5dd880cbf4ced",
+    "url": "/static/media/16-wittywalrus.7079ad99.svg"
+  },
+  {
+    "revision": "7cae14ae0982a53ee0bf64291a25db4d",
+    "url": "/static/media/17-beardedbear.7cae14ae.svg"
+  },
+  {
+    "revision": "f42f785e5f20aabc7496ba16c0b01821",
+    "url": "/static/media/18-cosmiccheetah.f42f785e.svg"
+  },
+  {
+    "revision": "95668afdbe68b832050046240802cd38",
+    "url": "/static/media/19-keenkangaroo.95668afd.svg"
+  },
+  {
+    "revision": "f4dc9dffb75f681cdd0b5bae220866e8",
+    "url": "/static/media/2-lawlesslion.f4dc9dff.svg"
+  },
+  {
+    "revision": "035b9ce6f7f293a09885af5053678422",
+    "url": "/static/media/20-deadlyduck.035b9ce6.svg"
+  },
+  {
+    "revision": "966ce5dfd8cc0f1cd9f8cbdf495bb58f",
+    "url": "/static/media/21-gaudygoose.966ce5df.svg"
+  },
+  {
+    "revision": "f89c12a07049e7dc743d778286d27982",
+    "url": "/static/media/22-leisurelylemur.f89c12a0.svg"
+  },
+  {
+    "revision": "b9054f11a27aad93def4cd3c47acfa60",
+    "url": "/static/media/23-oddostrich.b9054f11.svg"
+  },
+  {
+    "revision": "6128472f48c3864bcb09de1ce11ab628",
+    "url": "/static/media/24-ominousowl.6128472f.svg"
+  },
+  {
+    "revision": "cd89c63d5c4024db2a9a3cdcf218c7eb",
+    "url": "/static/media/25-bipolarboar.cd89c63d.svg"
+  },
+  {
+    "revision": "d7c1c455647606a2e15ca70203841c5a",
+    "url": "/static/media/26-pickypenguin.d7c1c455.svg"
+  },
+  {
+    "revision": "06f440c22c669bacd52e24e406a67347",
+    "url": "/static/media/27-cageycamel.06f440c2.svg"
+  },
+  {
+    "revision": "7aac2eba5f9f25d755c9b1e7f90b7692",
+    "url": "/static/media/28-rationalraccoon.7aac2eba.svg"
+  },
+  {
+    "revision": "0d2070fa4c6f12e34eabc0f508e4ef49",
+    "url": "/static/media/29-heavyhippo.0d2070fa.svg"
+  },
+  {
+    "revision": "b7f54545fb5c801f38a39d8526822c6e",
+    "url": "/static/media/3-terrifictiger.b7f54545.svg"
+  },
+  {
+    "revision": "e1578c71a4cb4c649b515ddc65ff3c3c",
+    "url": "/static/media/30-manicmonkey.e1578c71.svg"
+  },
+  {
+    "revision": "06311eee524593d078a987a697444af0",
+    "url": "/static/media/31-martialmeerkat.06311eee.svg"
+  },
+  {
+    "revision": "ab09f49fe3a95ad283a2a25d24f91930",
+    "url": "/static/media/32-slitheringsnake.ab09f49f.svg"
+  },
+  {
+    "revision": "36734aad60319ec9286a9381d537ec4c",
+    "url": "/static/media/33-zestyzebra.36734aad.svg"
+  },
+  {
+    "revision": "5b39baa2bea1afaeee56aa810db233e8",
+    "url": "/static/media/34-defunctdonkey.5b39baa2.svg"
+  },
+  {
+    "revision": "1ce828d6a17a380fefa21a4d57b70030",
+    "url": "/static/media/35-blindbull.1ce828d6.svg"
+  },
+  {
+    "revision": "de167e4fd9bd0bef379f2ad3105e16ba",
+    "url": "/static/media/36-gigglygoat.de167e4f.svg"
+  },
+  {
+    "revision": "98a1fd55552968db70b9edc393d3a55f",
+    "url": "/static/media/37-giftedgoat.98a1fd55.svg"
+  },
+  {
+    "revision": "839ddd2822d8994af529fa22d7f3f5dc",
+    "url": "/static/media/38-handyhorse.839ddd28.svg"
+  },
+  {
+    "revision": "1bfb1da3a36212c8f66a4860b094cd89",
+    "url": "/static/media/39-wackywolf.1bfb1da3.svg"
+  },
+  {
+    "revision": "24e1840607490a6a6a33790e156a037f",
+    "url": "/static/media/4-bafflingbear.24e18406.svg"
+  },
+  {
+    "revision": "d9ae3bd76073655c3616bc9c3f812de3",
+    "url": "/static/media/40-kindkoala.d9ae3bd7.svg"
+  },
+  {
+    "revision": "e4ee2db18a21522bdc3e16f609181514",
+    "url": "/static/media/41-highhedgehog.e4ee2db1.svg"
+  },
+  {
+    "revision": "1149fc423d0cc514ab906ec1a4d9acc4",
+    "url": "/static/media/42-futilefrog.1149fc42.svg"
+  },
+  {
+    "revision": "6f0c16f5742dbea16da4d1fda43d7321",
+    "url": "/static/media/43-timelessturtle.6f0c16f5.svg"
+  },
+  {
+    "revision": "f2d27775cb14ce2533150a8a0bc157f4",
+    "url": "/static/media/44-gentlegorilla.f2d27775.svg"
+  },
+  {
+    "revision": "95648a7f48f64cddcec9cf1c09c95d4c",
+    "url": "/static/media/45-giantgiraffe.95648a7f.svg"
+  },
+  {
+    "revision": "ef797e5004100ee798bf3ad05f186f34",
+    "url": "/static/media/46-dashingdeer.ef797e50.svg"
+  },
+  {
+    "revision": "1501a1f8544dbc65fa82d75f3f7bb9a8",
+    "url": "/static/media/47-rowdyrhino.1501a1f8.svg"
+  },
+  {
+    "revision": "a8d663f84daaecb257c2dd80d4abbee5",
+    "url": "/static/media/48-eliteelephant.a8d663f8.svg"
+  },
+  {
+    "revision": "98069339b703f1e62e5bbbbddc979609",
+    "url": "/static/media/49-paceypuma.98069339.svg"
+  },
+  {
+    "revision": "1e5b13a9335051b634c1ff0205859dbb",
+    "url": "/static/media/5-paltryparrot.1e5b13a9.svg"
+  },
+  {
+    "revision": "45843aee1a222c5f35f2bd31909b0fc1",
+    "url": "/static/media/50-finickyfox.45843aee.svg"
+  },
+  {
+    "revision": "b79fe825799b6a67b764f0edadbe58f5",
+    "url": "/static/media/6-radicalrabbit.b79fe825.svg"
+  },
+  {
+    "revision": "3634944bffda94325823bb0e05d0de7e",
+    "url": "/static/media/7-callouschameleon.3634944b.svg"
+  },
+  {
+    "revision": "ec586c194953551c4e32e37843bea253",
+    "url": "/static/media/8-slowsloth.ec586c19.svg"
+  },
+  {
+    "revision": "28f102e7f24c81dd747d94e2a46707a2",
+    "url": "/static/media/9-eccentricelk.28f102e7.svg"
+  },
+  {
+    "revision": "a7dc865e63d73d4a751b5f92f35dd6f8",
+    "url": "/static/media/Logo.a7dc865e.svg"
+  },
+  {
+    "revision": "be404d4a4a5f260dd7f988137fe3eece",
+    "url": "/static/media/Public_Room.be404d4a.svg"
+  },
+  {
+    "revision": "5469e3a492e5f77a7e6d10a0ae80104a",
+    "url": "/static/media/copylink.5469e3a4.svg"
+  },
+  {
+    "revision": "68148e03ea569001aa1ecd47e201471f",
+    "url": "/static/media/hidden.68148e03.svg"
+  },
+  {
+    "revision": "daeb911b4e633af1c1d04284c39435c6",
+    "url": "/static/media/lock.daeb911b.svg"
+  },
+  {
+    "revision": "66dc041ad16c2e57a21aaec36c76c393",
+    "url": "/static/media/user.66dc041a.svg"
+  },
+  {
+    "revision": "58c6db7c7edd10ae68650c7fec472161",
+    "url": "/static/media/whatsapp.58c6db7c.svg"
+  }
+]);
