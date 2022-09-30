0840656bbfc107a646790998b84c7bd3c032e452
Mark Clayton <mark.clayton93@gmail.com>
2022-09-30 16:11:45 -0500

    reset axios

--- client/utils/axios-utils.js
+++ client/utils/axios-utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const IP = "localhost";
-export const PORT = 8872;
+export const PORT = 8882;
 
 export const request = axios.create({
   baseURL: `http://${IP}:${PORT}`,
